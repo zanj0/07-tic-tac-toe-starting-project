0cb08ee4f45d56e1dddaa0b87ea95d0c9c44d46f
Agnibha Chakraborty <agnichak@amazon.com>
2024-06-21 18:20:09 +0530

    Disable selecting same button multiple times

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,7 +33,10 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, columnIndex) => (
               <li key={columnIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, columnIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, columnIndex)}
+                  disabled={playerSymbol != null}
+                >
                   {playerSymbol}
                 </button>
               </li>
