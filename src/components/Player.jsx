4403296da855d2873eefd32aa62bd352845e17bc
Agnibha Chakraborty <agnichak@amazon.com>
2024-06-21 16:01:33 +0530

    Update player name edit button name on click

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,13 +3,15 @@ export default function Player({ name, symbol }) {
   const [isEditting, setIsEditting] = useState(false);
 
   function handleEditClick() {
-    setIsEditting(true);
+    setIsEditting(!isEditting);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  //   let btnCaption = "Edit";
 
   if (isEditting) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = "Save";
   }
 
   return (
@@ -18,7 +20,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditting ? "Save" : "Edit"}</button>
     </li>
   );
 }
